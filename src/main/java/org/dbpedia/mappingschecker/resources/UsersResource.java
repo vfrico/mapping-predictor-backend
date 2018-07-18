84fa2e0723fcdf2bc7d1a2262c2228628d3e2048
Víctor Fernández <vfrico@gmail.com>
2018-07-18 16:19:58 +0200

    Fixed add user method

--- src/main/java/org/dbpedia/mappingschecker/resources/UsersResource.java
+++ src/main/java/org/dbpedia/mappingschecker/resources/UsersResource.java
@@ -5,16 +5,19 @@ import es.upm.oeg.tools.mappings.SQLAnnotationReader;
 import es.upm.oeg.tools.mappings.beans.ApiError;
 import org.dbpedia.mappingschecker.util.Utils;
 import org.dbpedia.mappingschecker.web.UserDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Path("/users")
 public class UsersResource {
+
+    private static Logger logger = LoggerFactory.getLogger(UsersResource.class);
 
     /**
      * Adds a new user to the system. If it exists, it responds with a 4xx response status code
@@ -24,13 +27,33 @@ public class UsersResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response addUser(UserDAO newUser) {
-        SQLAnnotationReader sql = new SQLAnnotationReader(Utils.getMySqlConfig());
+        newUser.setId(0);
+        newUser.setCreation_date(new Timestamp(System.currentTimeMillis()));
+
+        String mysqlConfig = "jdbc:"+Utils.getMySqlConfig();
+        SQLAnnotationReader sql = new SQLAnnotationReader(mysqlConfig);
         UserDAO dbUser =  sql.getUser(newUser.getUsername());
 
-        if (dbUser != null) {
+        logger.info("dbUser: "+dbUser);
+        if (dbUser == null) {
             boolean res = sql.addUser(newUser);
+            UserDAO addedUser = sql.getUser(newUser.getUsername());
             if (res) {
-                return Response.status(201).entity(newUser).build();
+                return Response.status(201).entity(addedUser).build();
+            }
+
+        } else {
+            ApiError err = new ApiError("The user "+newUser.getUsername()+" already exists on DB", 412);
+            return  err.toResponse().build();
+        }
+        ApiError err = new ApiError("The user could not been created", 500);
+        return err.toResponse().build();
+    }
+
+            boolean res = sql.addUser(newUser);
+            UserDAO addedUser = sql.getUser(newUser.getUsername());
+            if (res) {
+                return Response.status(201).entity(addedUser).build();
             }
 
         } else {
