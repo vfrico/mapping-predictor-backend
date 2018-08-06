768c22ddfb6c89f2afc264d1377ed2c2211b56dd
Víctor Fernández <vfrico@gmail.com>
2018-08-06 12:38:22 +0200

    Updated SQL schema

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -19,20 +19,20 @@ USE `mappings_annotations`;
 -- Volcando estructura para tabla mappings_annotations.annotation
 CREATE TABLE IF NOT EXISTS `annotation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `langA` varchar(3) COLLATE utf8_bin DEFAULT NULL COMMENT 'Language A (use ISO code of two digits when possible)',
+  `langA` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'Language A (use ISO code of two digits when possible)',
   `langB` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'Language B (use ISO code of two digits when possible)',
-  `templateA` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `templateB` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `attributeA` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `attributeB` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `propertyA` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `propertyB` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `classA` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `classB` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `propDomainA` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `propDomainB` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `propRangeA` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `propRangeB` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `templateA` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `templateB` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `attributeA` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `attributeB` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `propertyA` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `propertyB` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `classA` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `classB` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `propDomainA` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `propDomainB` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `propRangeA` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `propRangeB` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `c1` float DEFAULT NULL,
   `c2` float DEFAULT NULL,
   `c3A` float DEFAULT NULL,
@@ -57,7 +57,22 @@ CREATE TABLE IF NOT EXISTS `annotation` (
   `tb11` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18606 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla mappings_annotations.classification_results
+CREATE TABLE IF NOT EXISTS `classification_results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_annotation` int(11) DEFAULT NULL COMMENT 'FK references annotation id',
+  `classified_as` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'References to vote types',
+  `probability` decimal(10,4) DEFAULT NULL COMMENT 'The probability that the classifier has estimated',
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_idannotation_classification` (`id_annotation`),
+  KEY `fk_votetype_classification` (`classified_as`),
+  CONSTRAINT `fk_idannotation_classification` FOREIGN KEY (`id_annotation`) REFERENCES `annotation` (`id`),
+  CONSTRAINT `fk_votetype_classification` FOREIGN KEY (`classified_as`) REFERENCES `vote_type` (`type`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla mappings_annotations.lock
@@ -66,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `lock` (
   `date_start` datetime DEFAULT NULL,
   `date_end` datetime DEFAULT NULL,
   `id_annotation` int(11) NOT NULL,
-  `username` varchar(45) COLLATE utf8_bin NOT NULL,
+  `username` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idlock`),
   KEY `fk_lock_1_idx` (`id_annotation`),
   KEY `fk_username` (`username`),
@@ -78,36 +93,36 @@ CREATE TABLE IF NOT EXISTS `lock` (
 -- Volcando estructura para tabla mappings_annotations.users
 CREATE TABLE IF NOT EXISTS `users` (
   `idusers` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `email` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `username` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `password_md5` varchar(512) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'Long enough to use any hashing algorithm\n',
   `creation_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `jwt` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `jwt` varchar(1024) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`idusers`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla mappings_annotations.vote
 CREATE TABLE IF NOT EXISTS `vote` (
-  `idVote` int(11) NOT NULL AUTO_INCREMENT,
+  `idvote` int(11) NOT NULL AUTO_INCREMENT,
   `annotation_id` int(11) NOT NULL,
   `creation_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `vote` varchar(20) COLLATE utf8_bin NOT NULL,
-  `username` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`idVote`),
+  `vote` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `username` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`idvote`),
   KEY `fk_vote_1_idx` (`annotation_id`),
   KEY `fk_vote_type` (`vote`),
   KEY `fk_vote_username` (`username`),
   CONSTRAINT `fk_annotation_id` FOREIGN KEY (`annotation_id`) REFERENCES `annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_vote_type` FOREIGN KEY (`vote`) REFERENCES `vote_type` (`type`),
   CONSTRAINT `fk_vote_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4694 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla mappings_annotations.vote_type
 CREATE TABLE IF NOT EXISTS `vote_type` (
-  `type` varchar(20) COLLATE utf8_bin NOT NULL,
+  `type` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
