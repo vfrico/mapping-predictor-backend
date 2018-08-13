8e3c59c771ecfb43da5260b30dfd5920f04514bf
Víctor Fernández <vfrico@gmail.com>
2018-08-14 01:35:31 +0200

    Adding license on files

--- src/main/java/es/upm/oeg/tools/mappings/SQLAnnotationReader.java
+++ src/main/java/es/upm/oeg/tools/mappings/SQLAnnotationReader.java
@@ -20,6 +20,24 @@ import org.dbpedia.mappingschecker.web.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Copyright 2018 Víctor Fernández <vfrico@gmail.com>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Víctor Fernández <vfrico@gmail.com>
+ * @since 1.0.0
+ */
 public class SQLAnnotationReader implements AnnotationReader {
 
     private static String test = "jdbc:mysql://localhost/dbpedia_mappings?user=dbpedia_mapping&password=dbpedia_mapping&serverTimezone=UTC";
---
+++ src/main/resources/csv/README.md
@@ -0,0 +1,18 @@
+# CSV annotations file
+Most of this files has been extracted from previous projects.
+They aren't generated specifically for this project, so they are
+not subjected to the Apache 2.0 license.
+
+The Copyrights are owned by the Ontology Engineering Group, 
+at the Universidad Politécnica de Madrid. Please, contact 
+them before making wrong use of it.
+
+https://github.com/oeg-upm
+
+At least, this list of people has collaborated on annotating
+each mapping.
+
+* https://github.com/nandana
+* https://github.com/MarianoRico
+* https://github.com/jimkont
+* https://github.com/vfrico
